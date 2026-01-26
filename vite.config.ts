44fe8bd53524694fe6408449139336eb3191b5b4
JWJung-99 <39busy@naver.com>
2026-01-26 20:21:04 +0900

    Conf: Alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@', replacement: '/src' },
+			{ find: '@assets', replacement: '/src/assets' },
+			{ find: '@components', replacement: '/src/components' },
+			{ find: '@pages', replacement: '/src/pages' },
+			{ find: '@hooks', replacement: '/src/hooks' },
+			{ find: '@store', replacement: '/src/store' },
+			{ find: '@styles', replacement: '/src/styles' },
+			{ find: '@stories', replacement: '/src/stories' },
+			{ find: '@tests', replacement: '/src/tests' },
+			{ find: '#types', replacement: '/src/types' },
+			{ find: '@utils', replacement: '/src/utils' },
+		],
+	},
+});
