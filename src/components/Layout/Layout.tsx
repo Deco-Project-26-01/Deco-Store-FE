a6d1979268c30d424b4973338229ecc3cb175145
JWJung-99 <39busy@naver.com>
2026-02-02 01:25:20 +0900

    Cont: 라우팅 경로 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import Header from '@components/Header/Header';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const Layout = () => {
+	const { pathname } = useLocation();
+	const isCheckoutPage = pathname.split('/')[1] === 'checkout';
+	console.log(isCheckoutPage);
+
 	return (
 		<>
+			<Header type={isCheckoutPage ? 'checkout' : 'default'} />
 			<Outlet />
 		</>
 	);
