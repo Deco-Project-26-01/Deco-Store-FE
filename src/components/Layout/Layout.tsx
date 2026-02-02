91d23167594f076d321d6716ac060b9e9c4036e4
JWJung-99 <39busy@naver.com>
2026-02-02 17:18:58 +0900

    Revert "Cont: 라우팅 경로 수정"
    This reverts commit a6d1979268c30d424b4973338229ecc3cb175145.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,8 @@
-import Header from '@components/Header/Header';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-	const { pathname } = useLocation();
-	const isCheckoutPage = pathname.split('/')[1] === 'checkout';
-	console.log(isCheckoutPage);
-
 	return (
 		<>
-			<Header type={isCheckoutPage ? 'checkout' : 'default'} />
 			<Outlet />
 		</>
 	);
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,8 +3,6 @@ import Layout from '@components/Layout/Layout';
 import Error from '@components/Error/Error';
 import Home from '@pages/Home/Home';
 import Cart from '@pages/Cart/Cart';
-import MyPage from '@pages/Mypage/Mypage';
-import Support from '@pages/Mypage/Support';
 
 const router = createBrowserRouter([
 	{
@@ -14,28 +12,13 @@ const router = createBrowserRouter([
 		children: [
 			// 홈
 			{
-				index: true,
+				path: '/',
 				element: <Home />,
 			},
 			// 장바구니
 			{
-				path: 'cart',
+				path: '/cart',
 				element: <Cart />,
-			},
-			// 마이페이지
-			{
-				path: 'mypage',
-
-				children: [
-					{
-						index: true,
-						element: <MyPage />,
-					},
-					{
-						path: 'support',
-						element: <Support />,
-					},
-				],
 			},
 		],
 	},
