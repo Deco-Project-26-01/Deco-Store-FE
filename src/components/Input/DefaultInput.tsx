02f1f9f57e8e07ebf8943cd0d298df581d42c33c
JWJung-99 <39busy@naver.com>
2026-02-12 16:42:26 +0900

    Feat: DefaultInput 컴포넌트 구현

--- src/components/Input/DefaultInput.tsx
+++ src/components/Input/DefaultInput.tsx
@@ -1,7 +1,70 @@
-const DefaultInput = ({}) => {
+import iconXCircleBase300 from '@assets/icons/icon-x-circle-base300.svg';
+import IconButton from '@components/Button/IconButton';
+import { type ComponentPropsWithoutRef, type ReactElement } from 'react';
+
+interface IDefaultInputProps extends ComponentPropsWithoutRef<'input'> {
+	passwordToggle?: ReactElement;
+	onIconClick: React.MouseEventHandler<HTMLButtonElement>;
+	description?: string;
+	isError: boolean;
+	errorMessage: string;
+}
+
+const DefaultInput = ({
+	value,
+	passwordToggle,
+	onIconClick,
+	description,
+	isError,
+	errorMessage,
+	...rest
+}: IDefaultInputProps) => {
 	return (
-		<div>
-			<input />
+		<div className="w-full">
+			<div
+				className={`
+					mb-xs
+					flex items-center gap-sm
+					pl-lg pr-md py-sm
+					rounded-xs
+					box-border
+					border-solid border-2 border-base300
+					focus-within:border-primaryDark
+					${isError ? 'border-alert focus-within:border-alert' : ''}
+			`}
+			>
+				<input
+					value={value}
+					className="grow ellipsis text-bodyBase placeholder:text-base500"
+					{...rest}
+				/>
+				{passwordToggle && passwordToggle}
+				{!!value ? (
+					<div
+						className={`
+							ml-auto
+							w-[2.4rem] h-[2.4rem] p-xs
+							duration
+							flex items-center justify-center
+							hover:scale-95 hover:rounded-xs hover:bg-base300/10
+							focus-within:rounded-xs focus-within:outline focus-within:outline-1 focus-within:outline-base300
+							active:scale-95 active:rounded-xs active:bg-base300/10
+						`}
+					>
+						<IconButton
+							iconPath={iconXCircleBase300}
+							iconAlt={'Clear'}
+							onClick={onIconClick}
+						/>
+					</div>
+				) : (
+					<div className="w-[2.4rem] h-[2.4rem] ml-auto" />
+				)}
+			</div>
+			{description && (
+				<p className="text-bodyXsmall text-base500">{description}</p>
+			)}
+			{isError && <p className="text-bodyXsmall text-alert">{errorMessage}</p>}
 		</div>
 	);
 };
--- src/stories/DefaultInput.stories.ts
+++ src/stories/DefaultInput.stories.ts
@@ -16,10 +16,14 @@ const meta = {
 			action: 'changed',
 			description: '입력 필드의 값 변경 이벤트 핸들러',
 		},
-		value: { control: 'text', description: '입력 필드의 현재 값, 50자 제한' },
+		placeholder: { control: 'text', description: '입력 전 기본 텍스트' },
+		value: { control: 'text', description: '입력 필드의 값' },
+		description: { control: 'text', description: '입력값의 조건 설명' },
+		isError: { control: 'boolean', description: '입력 값의 에러 여부' },
+		errorMessage: { control: 'text', description: '에러 메시지' },
 		onIconClick: {
 			action: 'clicked',
-			description: '아이콘 클릭 이벤트 핸들러',
+			description: '입력 필드 초기화 아이콘 클릭 이벤트 핸들러',
 		},
 	},
 	args: { onChange: fn(), onIconClick: fn() },
@@ -28,8 +32,33 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default_Without_Error: Story = {
 	args: {
-		value: 'Something',
+		placeholder: 'Placeholder',
+		value: '',
+		isError: false,
+		errorMessage: 'Error Message',
+	},
+};
+
+export const Default_With_Error: Story = {
+	args: {
+		type: 'email',
+		placeholder: 'Placeholder',
+		value: 'stevekim123naver.com',
+		isError: true,
+		errorMessage: 'E-mail does not meet the requirements.',
+	},
+};
+
+export const Default_With_Description: Story = {
+	args: {
+		type: 'password',
+		placeholder: 'Placeholder',
+		value: 'qwer1234',
+		description:
+			'Your password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character (e.g. ! @ # $ %).',
+		isError: false,
+		errorMessage: 'Error Message',
 	},
 };
