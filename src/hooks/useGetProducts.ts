77f8c104b6fd0f88c007ae55b28548bde09be496
JWJung-99 <39busy@naver.com>
2026-03-22 23:59:06 +0900

    Feat: 상품 조회 API 연동 및 스켈레톤 UI 추가

---
+++ src/hooks/useGetProducts.ts
@@ -0,0 +1,50 @@
+import type {
+	IProductListFailureResponse,
+	IProductListSuccessResponse,
+} from '#types/products';
+import useCustomAxios from '@hooks/useCustomAxios';
+import { useQuery } from '@tanstack/react-query';
+import { isAxiosError, type AxiosInstance } from 'axios';
+
+const fetchProducts = async (
+	pageNum: number,
+	params: string,
+	instance: AxiosInstance,
+) => {
+	try {
+		const response = await instance.get<IProductListSuccessResponse>(
+			`/products?size=8${pageNum > 0 ? `&page=${pageNum}` : ''}${params ? `&${params}` : ''}`,
+		);
+		return response.data;
+	} catch (error) {
+		if (isAxiosError<IProductListFailureResponse>(error)) {
+			if (error.response?.status === 401) {
+				throw new Error('Unauthorized. Please login again.');
+			}
+		}
+
+		throw new Error('Failed to fetch products. Please try again.');
+	}
+};
+
+const useGetProducts = (pageNum: number = 0, params: string = '') => {
+	const axios = useCustomAxios();
+
+	return useQuery({
+		queryKey: ['products', { pageNum, params }],
+		queryFn: () => fetchProducts(pageNum, params, axios),
+		select: (response) => ({
+			products: response.data.content,
+			pageInfo: {
+				currentPage: response.data.currentPage,
+				totalPages: response.data.totalPages,
+				totalElements: response.data.totalElements,
+				size: response.data.size,
+				hasNext: response.data.hasNext,
+				hasPrevious: response.data.hasPrevious,
+			},
+		}),
+	});
+};
+
+export default useGetProducts;
